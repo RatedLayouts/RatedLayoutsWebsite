a185304682eda50ab1b0ad48d0e717dded0ba13c
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2026-05-19 21:19:10 +0800

    no more vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   output: 'static',
   vite: {
     server: {
