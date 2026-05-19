114b65938a800bd1d8f38168e4c1783d5983a6ee
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2026-05-19 21:26:19 +0800

    moving to cloudflare page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   output: 'static',
+  adapter: cloudflare(),
   vite: {
     server: {
       proxy: {
